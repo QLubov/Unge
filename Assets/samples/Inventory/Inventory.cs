3e7522defdb08859c30eb883f2f53c633d70ef82
QLubov <QLubov>
2015-05-12 22:58:33 +0300

    initial commit

---
+++ Assets/samples/Inventory/Inventory.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+
+public class Inventory : MonoBehaviour {
+	public Texture2D CellTexture;
+	public int CellCount;
+	List<CollectableItem> mItems = new List<CollectableItem>();
+
+	void Start () {
+		
+	}
+	void OnGUI() {
+		DrawInventory ();
+		DrawInventoryItems ();
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+
+	private void DrawInventory () {
+		if (CellTexture == null)
+			return;
+		Vector3 coord = GetScreenSpace (transform.position);
+		for (int i = 0; i < CellCount; i++) {
+			GUI.DrawTexture(new Rect(coord.x + i*CellTexture.width, coord.y, CellTexture.width, CellTexture.height), CellTexture);
+		}
+	}
+
+	void DrawInventoryItems () {
+		Vector3 coord = GetScreenSpace (transform.position);
+		for (int i = 0; i < mItems.Count; i++) {
+			Texture2D itemImage = mItems [i].texture;
+			GUI.DrawTexture (new Rect (coord.x + i * CellTexture.width, coord.y, itemImage.width, itemImage.height), itemImage);
+		}
+	}
+
+	void OnDrawGizmos () {
+		DrawInventory ();
+	}
+
+	public static Vector3 GetScreenSpace (Vector3 position) {
+		Vector3 coord = Camera.main.WorldToScreenPoint (position);
+		coord.y = Screen.height - coord.y;
+		return coord;
+	}
+
+	public void Add (CollectableItem item) {
+		if(mItems.Count < CellCount)
+			mItems.Add (item);
+	}
+
+}
---
+++ Assets/samples/SwitchManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SwitchManager : MonoBehaviour {
+	public string TargetScene = "";
+
+	public void SwitchScene(){
+		//BinaryFormatter binFormat = new BinaryFormatter();
+		// Сохранить объект в локальном файле.
+		//using(Stream fStream = new FileStream("user.dat", FileMode.Create, FileAccess.Write, FileShare.None))
+		//{
+		//	binFormat.Serialize(fStream, GameObject.Find("Inventory").GetComponent<InventoryView>());
+		//}
+		Application.LoadLevel(TargetScene);
+	}
+}
