d8ec36dc3dc0a237ff1a98051e5f74a3d6becbda
QLubov <QLubov>
2015-05-13 23:04:08 +0300

    collectable items fixed

--- Assets/samples/GameObject/CollectableItem.cs
+++ Assets/samples/GameObject/CollectableItem.cs
@@ -5,28 +5,21 @@ public class CollectableItem : MonoBehaviour {
 	public Texture2D texture;
 	SpriteRenderer mSpriteRenderer;
 	Inventory mInventory;
-
+	
 	// Use this for initialization
 	void Start () {
 		mSpriteRenderer = GetComponent<SpriteRenderer> ();
 		mInventory = GameObject.Find ("Inventory").GetComponent<Inventory>();
 	}
-	void OnGUI () {
-		Rect rect = mSpriteRenderer.sprite.rect;
-		rect.position = Inventory.GetScreenSpace(transform.position);
-		GUI.Label (rect, "BLABLABLA!!1");
-	}
 	// Update is called once per frame
 	void Update () {
-		if (mSpriteRenderer.enabled && Input.GetMouseButtonDown (0)) {
-			Rect rect = mSpriteRenderer.sprite.rect;
-			rect.position = Inventory.GetScreenSpace(transform.position);
-			rect.position -= new Vector2(rect.width/2, rect.height/2);
-			if(rect.Contains(Input.mousePosition))
-			{
-				mInventory.Add(this);
-				mSpriteRenderer.enabled = false;
-			}
+	}
+
+	void OnMouseDown () {
+	
+		if (mSpriteRenderer.enabled) {
+			mSpriteRenderer.enabled = false;
+			mInventory.Add(this);
 		}
 	}
 }
