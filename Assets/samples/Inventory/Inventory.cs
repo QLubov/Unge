07700e172e9e980f45e4f5b145c64e6a0be441ec
QLubov <QLubov>
2015-05-24 01:40:00 +0300

    Inventory items stack added

--- Assets/samples/Inventory/Inventory.cs
+++ Assets/samples/Inventory/Inventory.cs
@@ -4,11 +4,24 @@ using System.Collections;
 using System.IO;
 using System.Collections.Generic;
 
+public class Pair<T, U> {
+	public Pair() {
+	}
+	
+	public Pair(T first, U second) {
+		this.First = first;
+		this.Second = second;
+	}
+	
+	public T First { get; set; }
+	public U Second { get; set; }
+};
 
 public class Inventory : MonoBehaviour {
+	public int labelOffset = 1;
 	public Texture2D CellTexture;
 	public int CellCount;
-	List<CollectableItem> mItems = new List<CollectableItem>();
+	List<Pair<CollectableItem, int>> mItems = new List<Pair<CollectableItem, int>>();
 
 	void Start () {
 		
@@ -33,8 +46,13 @@ public class Inventory : MonoBehaviour {
 	void DrawInventoryItems () {
 		Vector3 coord = GetScreenSpace (transform.position);
 		for (int i = 0; i < mItems.Count; i++) {
-			Texture2D itemImage = mItems [i].texture;
-			GUI.DrawTexture (new Rect (coord.x + i * CellTexture.width, coord.y, itemImage.width, itemImage.height), itemImage);
+			Texture2D itemImage = mItems [i].First.texture;
+			Rect pos = new Rect (coord.x + i * CellTexture.width, coord.y, itemImage.width, itemImage.height);
+
+			GUI.DrawTexture (pos, itemImage);
+
+			pos.position += new Vector2(0, labelOffset);
+			GUI.Label(pos, "" + mItems[i].Second);
 		}
 	}
 
@@ -49,8 +67,15 @@ public class Inventory : MonoBehaviour {
 	}
 
 	public void Add (CollectableItem item) {
-		if(mItems.Count < CellCount)
-			mItems.Add (item);
+		if (mItems.Count >= CellCount)
+			return;
+		for (int i = 0; i < mItems.Count; i++) {
+			if (item.gameObject.name == mItems[i].First.gameObject.name){
+				mItems[i].Second++;
+				return;
+			}
+		}
+		mItems.Add (new Pair<CollectableItem, int> (item, 1));
 	}
 
 }
