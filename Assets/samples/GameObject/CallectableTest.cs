1080ab2b358bb3c14ebcfe8824af2310e5010eae
QLubov <QLubov>
2015-06-01 23:15:12 +0300

    Inventory items, labels and object were added

---
+++ Assets/samples/GameObject/CallectableTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallectableTest : MonoBehaviour {
+    public Sprite collected;
+    InventoryTest inv;
+	// Use this for initialization
+	void Start ()
+    {
+        inv = GameObject.Find("InventoryTest").GetComponent<InventoryTest>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+    void OnMouseDown()
+    {
+        inv.AddItem(collected);
+        Destroy(gameObject);
+    }
+}
--- Assets/samples/Inventory/InventoryTest.cs
+++ Assets/samples/Inventory/InventoryTest.cs
@@ -1,40 +1,114 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
 
-public class InventoryTest : MonoBehaviour {
+public class InventorySlot
+{
+    public GameObject Slot { get; private set; }
+    public uint Count { get; private set; }
+    GameObject mItem;
+    float mCellSize;
+    float mScaleFactor;
+
+    public InventorySlot(GameObject slotPref, float offset, float cellSize, float position, float ScaleFactor = 14.0f)
+    {
+        Slot = GameObject.Instantiate<GameObject>(slotPref);
+
+        RectTransform slotRect = Slot.GetComponent<RectTransform>();
+        slotRect.localPosition = new Vector3(offset + position * (offset + cellSize) + cellSize / 2.0f, 0);
+        slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, cellSize);
+        slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, cellSize);
+        mCellSize = cellSize;
+        mScaleFactor = ScaleFactor;
+    }
+
+    bool IsEmpty()
+    {
+        return Count == 0;
+    }
+
+    public bool AddItem(Sprite sprite)
+    {
+        if (mItem != null)
+        {
+            var img = mItem.GetComponent<Image>();
+            if (img.sprite.GetHashCode() == sprite.GetHashCode())
+            {
+                Count++;
+                UpdateLabel();
+                return true;
+            }
+            return false;
+        }
+        else
+        {
+            mItem = new GameObject("Item");
+                       
+            var img = mItem.AddComponent<Image>();
+
+            var rect = mItem.GetComponent<RectTransform>();
+            rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, mCellSize - mScaleFactor);
+            rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, mCellSize - mScaleFactor);
+            
+
+            img.sprite = sprite;
+            Slot.GetComponent<Button>().targetGraphic = img;
+            mItem.transform.SetParent(Slot.transform, false);
+            mItem.transform.SetAsFirstSibling();
+            Count++;
+            UpdateLabel();
+            return true;
+        }
+    }
+
+    void UpdateLabel()
+    {
+        Slot.GetComponentInChildren<Text>().text = Convert.ToString(Count);
+    }
+}
+
+public class InventoryTest : MonoBehaviour
+{
     [Range(0.0f, 10.0f)]
     public float offset = 0.0f;
+    public float leftOffset = 0.0f;
     public float cellSize = 64.0f;
     public int cellCount = 4;
     public GameObject slotPref = null;
-    List<GameObject> slots = new List<GameObject>();
+    public GameObject itemPref = null;
+    List<InventorySlot> slots = new List<InventorySlot>();
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-        float invWidth = cellCount * cellSize + cellCount * offset + offset;
+        float invWidth = 2 * leftOffset + cellCount * cellSize + cellCount * offset + offset;
         float invHeight = cellSize + 2 * offset;
         RectTransform rect = GetComponent<RectTransform>();
+
+        for (float i = -cellCount / 2.0f; i < cellCount / 2.0f; i += 1.0f)
+        {
+            var slot = new InventorySlot(slotPref, offset, cellSize, i);
+
+            slot.Slot.transform.SetParent(transform, false);
+            slots.Add(slot);
+        }
         rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, invWidth);
         rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, invHeight);
+    }
 
-        for (int i = 0; i < cellCount; i++)
+    public void AddItem(Sprite sprite)
+    {
+        foreach (var slot in slots)
         {
-            GameObject slot = Instantiate<GameObject>(slotPref);
-            
-
-            RectTransform slotRect = slot.GetComponent<RectTransform>();
-            //slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, cellSize);
-            //slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, cellSize);
-            slot.transform.parent = transform;
-            //slotRect.localPosition = rect.localPosition + new Vector3(offset + i * cellSize, 0);
-            //
+            if (slot.AddItem(sprite))
+                break;
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
