b967e2b7bf101d662f6604affbca5bdd9dd46d23
QLubov <romashishka@bk.ru>
2015-06-03 16:39:53 +0300

    scene 2 was changed

---
+++ Assets/samples/Switcher.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switcher : MonoBehaviour {
+	public string TargetScene = "";
+
+	void OnMouseDown()
+	{
+		Application.LoadLevel(TargetScene);
+	}
+}
