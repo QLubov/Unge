b6c2ef562df4fec0f11b49db8de33db28f21846a
QLubov <romashishka@bk.ru>
2015-06-05 16:17:55 +0300

    Toggle object was added

---
+++ Assets/samples/GameObject/ToggleObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleObject : MonoBehaviour {
+	public Sprite State1 = null;
+	public Sprite State2 = null;
+	SpriteRenderer mRenderer;
+	Sprite prev = null;
+	Sprite next = null;
+	// Use this for initialization
+	void Start ()
+	{
+		mRenderer = GetComponent<SpriteRenderer> ();
+		mRenderer.sprite = State1;
+		next = State2;
+		prev = State1;
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown () 
+	{
+		mRenderer.sprite = next;
+		next = prev;
+		prev = mRenderer.sprite;
+	}
+}
