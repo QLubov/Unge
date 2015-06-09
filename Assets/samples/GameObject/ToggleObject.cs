f572340e81c0f5a9977125c3953ced578ec9c260
QLubov <QLubov>
2015-06-09 21:47:23 +0300

    Loading & toggling

---
+++ Assets/samples/GameObject/ToggleObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleObject : MonoBehaviour {
+
+    public Sprite state1 = null;
+    public Sprite state2 = null;
+    SpriteRenderer rnd;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rnd = GetComponent<SpriteRenderer>();
+        rnd.sprite = state1;
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown ()
+    {
+        if (rnd.sprite == state1)
+            rnd.sprite = state2;
+        else
+            rnd.sprite = state1;
+	}
+}
---
+++ Assets/samples/InventoryTestLoad.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryTestLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        var inv = GameObject.Find("InventoryTest");
+        inv.transform.SetParent(transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
